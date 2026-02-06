dcd6569e3f8e0c3637dd4f9a57ed56daa4816721
Pedro Ramírez <pedrolex.ramirez@gmail.com>
2026-02-06 12:23:18 -0600

    enviar productos y almacenarlos en una lista

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,18 @@
+let products = []
 class Product {
-    constructor(name, description){
+    constructor(name, description, price, img){
         this._name = name;
         this._description = description;
+        this._price = price;
     }
+}
+
+function enviar() {
+    let name = document.getElementById("").value
+    let description = document.getElementById("").value
+    let price = document.getElementById("").value
+    let img = document.getElementById("").value
+
+    let product = new Product(name, description, price)
+    products.push(product)
 }
